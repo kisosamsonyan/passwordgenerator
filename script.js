8de2a21417bf19d16e24c9c710e4a9e282b98ecf
Kristine Samsonyan <kisosamsonyan@yahoo.com>
2019-10-18 00:47:07 -0700

    draft 1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+//Generate random password
+function generate(){
+
+//Set Password Length
+    var passLength = 8-128;
+    
+//Possible Password Values
+    let values= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let password="";
+
+//Create for loop to choose password characters
+    for(var i =0; i <= passLength; i++){
+        password= password
+    }
+}
+
 
